66e63ef0fd511018cb4316ec526f7a9f28bcaf84
Nathan O'Brien <nathan.ndmarketing@gmail.com>
2023-02-09 17:01:56 +0000

    Added Responsiveness

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,67 +1,69 @@
 // Defining the Calculator class object
 class Calculator {
-    // created via this, and given previous and current values
-  constructor(prevnumtext, currentnumtext) {
-    this.prevnumtext = prevnumtext;
-    this.currentnumtext = currentnumtext;
-    // clearing the values on creation
-    this.clear();
-  }
-  // clearing all values
-  clear() {
-    this.currentoperand = "";
-    this.previousoperand = "";
-    this.operation = undefined;
-  }
-  // appending the numbers to the current num, to a string
-  appendnum(number) {
-    if (number === "." && this.currentoperand.includes(".")) return;
-    this.currentoperand = this.currentoperand.toString() + number.toString();
-  }
-
-  chooseop(operation) {
-    if (this.currentoperand == "") return;
-    if (this.previousoperand !== "") {
-      this.compute();
-    }
-    
-    this.operation = operation;
-    this.previousoperand = this.currentoperand;
-    this.currentoperand = "";
-  }
-  // calculations Part
-  compute() {
-    let computation;
-    let prev = parseFloat(this.previousoperand);
-    let current = parseFloat(this.currentoperand);
-    if (isNaN(prev) || isNaN(current)) return;
-
-    switch (this.operation) {
-      case "+":
-        computation = prev + current;
-        break;
-      case "-":
-        computation = prev - current;
-        break;
-      case "x":
-        computation = prev * current;
-        break;
-      case "/":
-        computation = prev / current;
-        break;
-      default:
-        return;
-    }
-    this.currentoperand = computation;
-    this.operation = undefined;
-    this.previousoperand = "";
-  }
-  // updating 
-  updatedisplay() {
-    this.currentnumtext.innerText = this.currentoperand;
-    this.prevnumtext.innerText = this.previousoperand;
-  }
+  // created via this, and given previous and current values
+constructor(prevnumtext, currentnumtext) {
+  this.prevnumtext = prevnumtext;
+  this.currentnumtext = currentnumtext;
+  // clearing the values on creation
+  this.clear();
 }
+// clearing all values
+clear() {
+  this.currentoperand = "";
+  this.previousoperand = "";
+  this.operation = undefined;
+}
+// appending the numbers to the current num, to a string
+appendnum(number) {
+  if (number === "." && this.currentoperand.includes(".")) return;
+  this.currentoperand = this.currentoperand.toString() + number.toString();
+}
+
+chooseop(operation) {
+  if (this.currentoperand == "") return;
+  if (this.previousoperand !== "") {
+    this.compute();
+  }
+  
+  this.operation = operation;
+  this.previousoperand = this.currentoperand;
+  this.currentoperand = "";
+}
+// calculations Part
+compute() {
+  let computation;
+  let prev = parseFloat(this.previousoperand);
+  let current = parseFloat(this.currentoperand);
+  if (isNaN(prev) || isNaN(current)) return;
+
+  switch (this.operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "x":
+      computation = prev * current;
+      break;
+    case "/":
+      computation = prev / current;
+      break;
+    default:
+      return;
+  }
+  this.currentoperand = computation;
+  this.operation = undefined;
+  this.previousoperand = "";
+}
+// updating 
+updatedisplay() {
+  this.currentnumtext.innerText = this.currentoperand;
+  this.prevnumtext.innerText = this.previousoperand;
+}
+}
+
+
 // Declarations
 const numbuttons = document.querySelectorAll(".bnum");
 const operatives = document.querySelectorAll(".opt");
@@ -71,6 +73,9 @@ const delbutton = document.getElementById("del");
 const equals = document.getElementById("equals");
 const calculator = new Calculator(prevnumtext, currentnumtext);
 
+
+
+// Even Listeners
 numbuttons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendnum(button.innerText);
@@ -91,3 +96,4 @@ equals.addEventListener("click", () => {
   calculator.compute();
   calculator.updatedisplay();
 });
+
